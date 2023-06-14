01f0427a36050114e6b9d1d2baf75a966401cc53
fantadrinker <yizhi.tsui@gmail.com>
2023-06-14 12:00:10 -0700

    fix uploader bug

--- storage/lambdas/src/getUploadUrl/app.ts
+++ storage/lambdas/src/getUploadUrl/app.ts
@@ -1,17 +1,18 @@
 // gets upload url for a video
 //
 import { Handler } from 'aws-lambda'
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const URL_EXPIRATION_SECONDS = 300
 const s3Client = new S3Client({ region: process.env.AWS_REGION })
 
 export const handler: Handler = event => {
-  const { sub, fileID } = event.queryStringParameters
-  const Key = `${sub}/${fileID}`
+  const { sub } = event.queryStringParameters
+  const randomId = Math.random().toString(36).substring(2, 15)
+  const Key = `${sub}/${randomId}`
 
-  const getCommand = new GetObjectCommand({
+  const getCommand = new PutObjectCommand({
     Bucket: process.env.RECORDINGS_BUCKET,
     Key,
   })
